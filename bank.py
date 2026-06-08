ee526d3f3e1277a04b3c776525bb0d041097d26b
mwemz105-tech <mwemz105@gmail.com>
2026-06-08 19:10:26 +0300

    first commit

---
+++ bank.py
@@ -0,0 +1,50 @@
+# TASK
+# TASK1
+class BankAccount:
+    def __init__(self, account_number, owner_name, balance, date_opened):
+        self.account_number = account_number
+        self.owner_name = owner_name
+        self.balance = balance
+        self.date_opened = date_opened
+
+# TASK2
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance: {self.balance}")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+        else:
+            print("Insufficient funds")
+
+    def check_balance(self):
+        print(f"Balance: {self.balance}")
+
+    def display_info(self):
+        print(f"Account: {self.account_number}, Owner: {self.owner_name}, Balance: {self.balance}, Opened: {self.date_opened}")
+
+    def close_account(self):
+        self.balance = 0
+        print(f"Account {self.account_number} closed. Balance set to 0.")
+
+
+# TASK3
+acc1 = BankAccount("A101", "Alice", 500, "2024-01-10")
+acc2 = BankAccount("A102", "Bob", 1000, "2024-02-15")
+
+# ACC1
+acc1.deposit(200)
+acc1.withdraw(100)
+acc1.check_balance()
+acc1.display_info()
+acc1.close_account()
+
+# ACC2
+acc2.deposit(50)
+acc2.withdraw(300)
+acc2.check_balance()
+acc2.display_info()
+acc2.close_account()
