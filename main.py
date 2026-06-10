dc5a55cf2f04b62bfd6122ba6a148874d1973b86
mwemz105-tech <mwemz105@gmail.com>
2026-06-10 18:35:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+
+#Flask Instance
+app = Flask(__name__)
+
+#index route
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+#products route
+@app.route("/products")
+def products():
+    return render_template('products.html')
+
+
+#sales route
+@app.route('/sales')
+def sales():
+    return render_template('sales.html')
+
+
+#stock route
+@app.route('/stock')
+def stock():
+    return render_template('stock.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+#run your application
+app.run()
+
