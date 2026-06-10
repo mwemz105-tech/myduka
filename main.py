fe1404919cdec6450b5c09da45640a9eeea00775
mwemz105-tech <mwemz105@gmail.com>
2026-06-10 20:33:42 +0300

    first commit

--- main.py
+++ main.py
@@ -21,6 +21,25 @@ def sales():
     return render_template('sales.html')
 
 
+#stock route
+@app.route('/stock')
+def stock():
+    return render_template('stock.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 #run your application
