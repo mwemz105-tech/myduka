ec4609fde31b62f1e702c8c470a18971c6c41229
mwemz105-tech <mwemz105@gmail.com>
2026-06-15 15:17:12 +0300

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
-from database import get_products
-from database import get_sales
-from database import get_stocks
+from flask import Flask, render_template, request, redirect, url_for
+from database import get_products,insert_products
+from database import get_sales,insert_sales
+from database import get_stocks, insert_stock
 
 #Flask Instance
 app = Flask(__name__)
@@ -18,6 +18,18 @@ def products():
     products_data = get_products()
     return render_template('products.html',products_data=products_data)
 
+@app.route('/add_products',methods=['GET','POST'])
+def add_products():
+    if request.method == 'POST':
+        product_name = request.form['p_name']
+        buying_price = request.form['b_price']
+        selling_price = request.form['s_price']
+
+        new_product = (product_name,buying_price,selling_price)
+        insert_products(new_product)
+        print("Product added successfully")
+    return redirect(url_for('products'))
+
 
 #sales route
 @app.route('/sales')
@@ -25,12 +37,36 @@ def sales():
     sales_data = get_sales()
     return render_template('sales.html', sales_data=sales_data)
 
+@app.route('/add_sales',methods=['GET','POST'])
+def add_sales():
+    if request.method == 'POST':
+        product_id = request.form['product_id']
+        quantity = request.form['quantity']
+        selling_price = request.form['selling_price']
+
+        new_sale = (product_id,quantity,selling_price)
+        insert_sales(new_sale)
+        print("Sale recorded successfully")
+    return redirect(url_for('sales'))
+
 
 #stocks route
 @app.route('/stocks')
 def stocks():
     stocks_data=get_stocks()
     return render_template('stocks.html',stocks_data=stocks_data)
+
+@app.route('/add_stocks',methods=['GET','POST'])
+def add_stocks():
+    if request.method == 'POST':
+        product_id = request.form['product_id']
+        quantity = request.form['quantity']
+
+        new_stock = (product_id, quantity)
+        insert_stock(new_stock)
+        print("Stock added successfully")
+    return redirect(url_for('stocks'))
+
 
 
 @app.route('/dashboard')
